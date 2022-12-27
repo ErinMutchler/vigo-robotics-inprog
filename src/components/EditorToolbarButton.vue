46a4563eba3ddb3c4bbbd816105a11112c02f75d
Erin Mutchler <mutchler.erin@gmail.com>
2022-12-27 13:13:07 -0800

    Initial Commit

---
+++ src/components/EditorToolbarButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-btn
+    :icon="props.icon"
+    @mouseover="isHovering = true"
+    @mouseleave="isHovering = false"
+    :class="
+      isHovering
+        ? ['bg-vcpl-white', 'color-vcpl-secondary']
+        : ['bg-vcpl-secondary', 'color-vcpl-white']
+    "
+  >
+    <slot></slot>
+    <v-tooltip activator="parent" location="top" :contained="true">{{
+      props.tooltip
+    }}</v-tooltip>
+  </v-btn>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  tooltip: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+
+const isHovering = ref(false);
+</script>
+
+<style scoped></style>
