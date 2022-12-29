934729573e3d41af21b0f6353946fa49faf5b5fc
Erin Mutchler <mutchler.erin@gmail.com>
2022-12-28 23:47:38 -0500

    Injected Blockly

--- src/views/BlocklyView.vue
+++ src/views/BlocklyView.vue
@@ -1,9 +1,35 @@
 <template>
+  <table id="blocklyTable">
+    <tr>
+      <td id="blocklyArea"></td>
+    </tr>
+  </table>
+  <div id="blocklyDiv"></div>
 </template>
 
 <script setup>
+import { blocklyService } from "@/services/BlocklyService";
+import {onMounted} from "vue";
+
+onMounted(() => {
+  const div = document.getElementById("blocklyDiv");
+  const area = document.getElementById("blocklyArea");
+  blocklyService.inject(div, area);
+});
+
 </script>
 
 <style scoped>
+#blocklyDiv {
+  position: absolute;
+}
 
+#blocklyTable {
+  height: calc(100vh - 8rem);
+  width: 100%;
+}
+
+#blocklyArea {
+  height: 99%;
+}
 </style>
