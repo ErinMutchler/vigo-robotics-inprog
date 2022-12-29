e2924f71e58ff2f2a426a6ec8735081ea7fb4068
Erin Mutchler <mutchler.erin@gmail.com>
2022-12-28 23:47:28 -0500

    Added blockly category styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { registerPlugins } from "@/plugins";
 import App from "@/App.vue";
 
 import "@/assets/styles/main.css";
+import "@/assets/styles/categories.css";
 
 const app = createApp(App);
 
